5819b48e695d81723cedd046a5d7b090e8dbea98
JonOlav95 <jo95holland@gmail.com>
2022-12-04 20:01:00 +0100

    Comments day 4

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -2,36 +2,54 @@ import numpy as np
 import pandas as pd
 
 
-def split_to_range(x):
-    start, stop = x.split("-")
+def split_to_range(cell):
+    """Cast the string hyphen range to a python range.
+
+    :param cell: A string annotating the range (e.g. '8-73').
+    :return: The string range as a python range.
+    """
+    start, stop = cell.split("-")
     return range(int(start), int(stop) + 1)
 
 
-def func(x, mode):
-    r_1 = split_to_range(x[0])
-    r_2 = split_to_range(x[1])
+def func_subset(row):
+    """Check for subsets in the pair.
 
-    if mode == "intersection":
-        return bool(set(r_1).intersection(r_2))
-    elif mode == "subset":
-        return set(r_1).issubset(r_2) or set(r_2).issubset(r_1)
+    :param row: Each row containing two string ranges.
+    :return: True if either range is a subset of the other.
+    """
+    r_1 = split_to_range(row[0])
+    r_2 = split_to_range(row[1])
 
-    return -1
+    return set(r_1).issubset(r_2) or set(r_2).issubset(r_1)
+
+
+def func_intersection(row):
+    """Check for intersection in the pair.
+
+    :param row: Each row containing two string ranges.
+    :return: True if there is an intersection between the ranges.
+    """
+    r_1 = split_to_range(row[0])
+    r_2 = split_to_range(row[1])
+
+    return bool(set(r_1).intersection(r_2))
 
 
 def part_1(arr):
-    result = np.apply_along_axis(func, -1, arr, "subset")
+    result = np.apply_along_axis(func_subset, -1, arr)
     subset_sum = result.sum()
     print(subset_sum)
 
 
 def part_2(arr):
-    result = np.apply_along_axis(func, -1, arr, "intersection")
+    result = np.apply_along_axis(func_intersection, -1, arr)
     intersection_sum = result.sum()
     print(intersection_sum)
 
 
 def main():
+    # Read the input file as a csv, then cast it to a numpy array
     df = pd.read_csv("day_4.csv", header=None)
     arr = df.values
 

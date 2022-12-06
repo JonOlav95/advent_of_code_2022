dbe8a1e797db81c4e08107ce7ef27787a5225628
jo95 <jo95holland@gmail.com>
2022-12-06 09:40:32 +0100

    commnets

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -1,12 +1,17 @@
 import pandas as pd
-from collections import Counter
 
 
 def calc(inp, unqiue_length):
+    """Find first string with unique characters of length unique_length in inp.
+
+    :param inp: The input string.
+    :param unqiue_length: Length of unique characters to identify.
+    :return: None, used to exit function.
+    """
     for i in range(len(inp)):
         s = inp[i: i + unqiue_length]
 
-        length = len(Counter(s))
+        length = len(set(s))
 
         if length == unqiue_length:
             print(i + unqiue_length)
@@ -14,6 +19,7 @@ def calc(inp, unqiue_length):
 
 
 def main():
+    # Read as dataframe and boil it down to the single string
     inp = pd.read_csv("day_6.txt", header=None).values[0, 0]
     calc(inp, 4)
     calc(inp, 14)

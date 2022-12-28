19fc2e86fef73c5350a4dac7dacf1445d00a3216
jo95 <jo95holland@gmail.com>
2022-12-28 01:55:40 +0100

    Dayy 16 cont

--- day_16/day_16_2.py
+++ day_16/day_16_2.py
@@ -5,33 +5,68 @@ import re
 import numpy as np
 
 
-class Graph:
-
-    def __init__(self):
-        self.node_dict = {}
-
-    def add_node(self, idx):
-        self.node_dict[idx] = []
-
-    def add_edge(self, frm, to, weight):
-        self.node_dict[frm].append([to, weight])
-
-
 class Node:
-    def __init__(self, idx, fr, neigh):
+    def __init__(self, idx, fr):
         self.idx = idx
         self.fr = fr
-        self.neigh = neigh
+        self.visited = False
+        self.open = False
+        self.neighbours = []
+
+    def add_neighbour(self, neighbour, dist):
+        self.neighbours.append([neighbour, dist])
+
+    def __eq__(self, other):
+        if self.idx == other:
+            return True
+        return False
 
 
-def func(row, graph):
+def create_neighbours(row, graph):
 
-    for node in row["leads_to"]:
-        graph.add_edge(row["valve"], node, 1)
+    node = [x for x in graph if x.idx == row["valve"]][0]
+
+    for lead in row["leads_to"]:
+        lead = [x for x in graph if x.idx == lead][0]
+        node.add_neighbour(lead, 1)
+
+
+def from_to(graph, from_node, to_node):
+    visited_nodes = [from_node]
+
+
+def rec(graph, cnode, acc_flow, minute):
+    for key, neighbours in graph.node_dict.items():
+        print("")
+    print("x")
+
+
+def create_node(row):
+    node = Node(row["valve"], int(row["flow_rate"]))
+    return node
+
+
+def move_neighbours(from_node, to_node):
+    if from_node == to_node:
+        return
+
+    neighbours = [subl for subl in to_node.neighbours if from_node not in subl]
+
+    if neighbours == to_node.neighbours:
+        return
+
+    from_neighbours = from_node.neighbours
+
+    for new_neigh in from_neighbours:
+        if new_neigh not in to_node.neighbours:
+            to_node.add_neighbour(new_neigh[0], new_neigh[1] + 1)
+
+    if to_node.fr == 0:
+        for neigh in to_node.neighbours:
+            move_neighbours(to_node, neigh[0])
 
 
 def main():
-
     df = pd.read_csv("sample.txt", header=None, names=["full_text"], sep="<>")
 
     df["flow_rate"] = df["full_text"].apply(lambda x: re.search(r"-?\d+", x).group())
@@ -39,42 +74,18 @@ def main():
     df["valve"] = df["full_text"].apply(lambda x: re.findall(r"\b[A-Z]{2}\b", x)[0])
     df["leads_to"] = df["full_text"].apply(lambda x: re.findall(r"\b[A-Z]{2}\b", x)[1:])
 
-    graph = Graph()
-    df["valve"].apply(lambda x: graph.add_node(x))
-    df.apply(func, graph=graph, axis=1)
+    graph = df.apply(create_node, axis=1).values
+    df.apply(create_neighbours, graph=graph, axis=1)
 
-    zero_fr = df[df["flow_rate"] == "0"]["valve"]
-    zero_fr = zero_fr[zero_fr != "AA"].values
 
-    for v in zero_fr:
-        t_nodes = graph.node_dict[v]
-        graph.node_dict.pop(v)
+    for node in graph:
+        if node.fr != 0:
+            continue
 
-        t_nodes = np.array(t_nodes)
-        t_nodes[:, 1] = t_nodes[:, 1].astype(int) + 1
-        t_nodes = t_nodes.tolist()
+        for n in graph:
+            move_neighbours(node, n)
 
-        for key, neighbours in graph.node_dict.items():
-
-            if key == v:
-                continue
-
-            neigh = list(compress(neighbours, [v in subl for subl in neighbours]))
-
-            if not neigh:
-                continue
-
-            neighbours = [subl for subl in neighbours if v not in subl]
-
-            for t in t_nodes:
-                if t[0] != key:
-                    neighbours.append(t)
-
-            graph.node_dict[key] = neighbours
-
-    
-
-    return
+    graph = [subl for subl in graph if subl.fr != 0]
 
 
 if __name__ == "__main__":
